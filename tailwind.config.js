e4706a7ea917dc038c5408a9bbd773340b436e8a
HP <fz.mansouri@esi-sba.dz>
2025-03-16 21:18:27 +0100

    change of structure of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Chemins corrects
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#52BD94",
+        secondary: "#FCC068",
+        dark: "#0E2B34",
+        transparentPrimary: "#52BD944D",
+      },
+    },
   },
   plugins: [],
-}
-
+};
